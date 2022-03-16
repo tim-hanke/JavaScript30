7a22e591c714300252eed4513307610d9523f729
Timothy Hanke <tim.hanke@gmail.com>
2022-03-16 05:44:46 -0400

    select relevant elements from index.html

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,11 @@
 /* Get Our Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const ranges = player.querySelectorAll(".player__slider");
+const skipButtons = player.querySelectorAll("[data-skip]");
 
 /* Build Our Functions */
 
