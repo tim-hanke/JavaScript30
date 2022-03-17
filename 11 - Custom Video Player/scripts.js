0d98408caf30dfa8e91f65eae859c278a3c66326
Timothy Hanke <tim.hanke@gmail.com>
2022-03-17 06:08:03 -0400

    feat: add progress bar functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -29,13 +29,33 @@ function handleRangeChange() {
   video[this.name] = this.value;
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 /* Hook Up Event Listeners */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updatePlayButton);
 video.addEventListener("pause", updatePlayButton);
+video.addEventListener("timeupdate", handleProgress);
+
 toggle.addEventListener("click", togglePlay);
+
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+
 ranges.forEach((range) => range.addEventListener("change", handleRangeChange));
 ranges.forEach((range) =>
   range.addEventListener("mousemove", handleRangeChange)
 );
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
