2e334319248f0d3df63c06c37df84673a9599a1c
Timothy Hanke <tim.hanke@gmail.com>
2022-03-30 06:59:18 -0400

    feat: draw video onto canvas element

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -19,4 +19,16 @@ function getVideo() {
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
 getVideo();
