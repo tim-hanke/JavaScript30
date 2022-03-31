bca1f40fb24e57b7efdfe447f990a0834cd186a0
Timothy Hanke <tim.hanke@gmail.com>
2022-03-31 06:47:06 -0400

    feat: implement first filter: redEffect

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -28,7 +28,41 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt="Webcam Image" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    // pixels is an array that contains elements for
+    // red, green, blue, alpha for each pixel of
+    // the image
+    // since i is moving by 4, i + 0 is red,
+    // i + 1 is green, etc
+    pixels.data[i + 0] = pixels.data[i + 0] + 150; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
+    // pixels[i + 3] = pixels[i + 3]; // ALPHA
+  }
+  return pixels;
+}
+
+// 23:55
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
